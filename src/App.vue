3a603a21f019b7624552c48372a22c35471ae23b
vivek k p <vivekkannathuparambu@gmail.com>
2024-07-12 23:01:04 +0530

    first commit created member ui

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,162 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <SideBar />
+    <div class="header">
+    </div>
+    <div class="page-content pt-4 ps-4 pe-4">
+      <div class="ps-5">
+        <div class=" ps-3  d-flex justify-content-end  mb-3">
+          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModalLong">
+            Add Member
+          </button>
+
+          <!-- Modal -->
+          <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog"
+            aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+              <div class="modal-content">
+                <div class="modal-header border-0">
+                  <h5 class="modal-title" id="exampleModalLongTitle">Add New Member</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body border-0 ">
+                  <form class=" w-100 p-2">
+                    <div class="d-flex">
+                      <span class="material-symbols-outlined text-primary">
+                        person
+                      </span>
+                      <input class="inputField w-100  border-end-0 border-start-0 border-top-0" id="name" type="text" v-model="userEmail"
+                        placeholder="Enter name"><br>
+                        </div>
+                        <div class="d-flex mt-4">
+                      <span class="material-symbols-outlined text-primary">
+                        email
+                      </span>
+                      <input class="inputField w-100 border-end-0 border-start-0 border-top-0" id="email" type="email" v-model="userEmail"
+                        placeholder="Enter email address"><br>
+
+                    </div>
+
+                    </form>
+                </div>
+                <div class="modal-footer  border-0">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div class="card shadow-sm">
+          <div class="card-body">
+            <table class="table table-hover">
+              <thead>
+                <tr>
+                  <th>Sl no</th>
+                  <th> Member Name</th>
+                  <th>Email</th>
+                  <th>Joined Date</th>
+                  <th>Edit</th>
+                  <th>Delete</th>
+
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="i in 10" :key="i">
+                  <td>{{ i }}</td>
+                  <td>name</td>
+                  <td>name@linways</td>
+                  <td>20/11/22</td>
+                  <td><a href="#" class="card-refresh" data-toggle="refresh"><span
+                        class="material-symbols-outlined fs-5 text-black">
+                        edit
+                      </span></a></td>
+                  <td><a href="#" class="card-close" data-toggle="close"><span
+                        class="material-symbols-outlined fs-5 ps-1 text-danger">
+                        delete
+                      </span></a></td>
+                </tr>
+              </tbody>
+            </table>
+
+          </div>
+          <!-- <div v-for="i in 10" :key="i" class="card mb-4 me-4" style="max-width: 18rem;">
+            <div class="card-header">
+              <div class="head-title">
+                Header
+              </div>
+              <div class="card-controls pt-1">
+                <ul>
+                  <li><a href="#" class="card-refresh" data-toggle="refresh"><span
+                        class="material-symbols-outlined fs-5 text-black">
+                        edit
+                      </span></a>
+                  </li>
+                  <li><a href="#" class="card-close" data-toggle="close"><span
+                        class="material-symbols-outlined fs-5 ps-1 text-danger">
+                        delete
+                      </span></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="card-body d-flex">
+              <div class="d-flex justify-content-center align-items-center bg-primary rounded-circle user-icon-card me-3">
+                <span class="material-symbols-outlined fs-3 text-light">
+                  person
+                </span>
+              </div>
+              <div class="d-flex flex-column align-items-start">
+                <h6 class="m-0">VIVEK</h6>
+                <p class="mb-0">vivek@gmail.com</p>
+              </div>
+            </div>
+          </div> -->
+          <!-- <div id="card-linear" class="card card-default">
+            <div class="card-header  ">
+              <div class="card-title">Portlet Title
+              </div>
+              <div class="card-controls">
+                <ul>  
+                  <li><a href="#" class="card-refresh" data-toggle="refresh"><span class="material-symbols-outlined">
+                        edit
+                      </span></a>
+                  </li>
+                  <li><a href="#" class="card-close" data-toggle="close"><span class="material-symbols-outlined">
+                        delete
+                      </span></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="card-body">
+              <h3>
+                <span class="semi-bold">Linear</span> Progress
+              </h3>
+              <p>Click on the refresh icon to simulate an AJAX call and to see an animated progress bar indicator above
+                the portlet. These progress bars come in seven dif!~ferent colors that are available in the Pages
+                contextual color scheme.
+              </p>
+            </div>
+          </div> -->
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SideBar from './components/SideBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SideBar
   }
 }
 </script>
@@ -22,5 +169,57 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+
+.header {
+  border-bottom: 1px solid rgb(221, 221, 221);
+  height: 60px;
+  margin-left: 80px;
+  position: sticky;
+  /* Make it sticky */
+  top: 0;
+  /* Stick it to the top */
+  z-index: 1000;
+  background-color: white
+}
+
+.page-content {
+  margin-left: 80px;
+}
+
+.card-controls {
+  float: right;
+  margin-top: -3px;
+
+}
+
+.head-title {
+  float: left;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+}
+
+ul li {
+  padding-left: 0px;
+  display: inline-block;
+  list-style: none;
+  line-height: 0;
+}
+
+.card-body {
+  float: right;
+}
+
+.user-icon-card {
+  border-radius: 10px;
+  width: 40px;
+  height: 40px;
+  background-color: #406ff3;
+
+
 }
 </style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,78 @@
+<template>
+    <nav class="page-sidebar">
+      <div class="sidebar-header">
+        <h1>ibfn</h1>
+      </div>
+      <div class="sidebar-menu nav-bar p-3   nav flex-column nav-pills">
+        <div class="card icon-card pt-2 pb-2 mb-4 mt-4">
+          <span class="material-symbols-outlined">
+            home
+          </span>
+        </div>
+        <div class="card icon-card pt-2 pb-2 mb-4">
+          <span class="material-symbols-outlined">
+            library_books
+          </span>
+        </div>
+        <div class="card icon-card pt-2 pb-2">
+          <span class="material-symbols-outlined">
+            person
+          </span>
+        </div>
+      </div>
+    </nav>
+
+</template>
+
+<script>
+export default {
+  name: 'SideBar',
+ 
+}
+</script>
+
+<style scoped>
+
+
+.nav-bar {
+  height: 100vh;
+} 
+
+
+element.style {
+  transform: translate3d(0px, 0px, 0px);
+}
+
+.page-sidebar {
+  width: 80px;
+  background-color: #2b303b;
+  position: fixed;
+  bottom: 0;
+  top: 0;
+  right: auto;
+  overflow: hidden;
+}
+
+.sidebar-header{
+  background-color: #272b35;
+  height: 60px;
+}
+
+.sidebar-menu {
+  height: calc(100% - 50px);
+  position: relative;
+  width: 100%;
+}
+
+.material-symbols-outlined {
+  color: white;
+}
+
+.icon-card {
+  background-color: #1f222a;
+  border-radius: 8px;
+
+}
+
+ 
+</style>
